2282e82aecf9a3dd505e335544cbc6171fb40ea6
TheSelfAtu <43713568+TheSelfAtu@users.noreply.github.com>
2023-01-15 18:41:45 +0900

    add: radio to select user (#5)
    * add: heading1
    
    * add: form Radio

--- majority-decision/src/pages/index.tsx
+++ majority-decision/src/pages/index.tsx
@@ -1,15 +1,20 @@
 import type { NextPage } from "next";
 import axios from "axios";
+import { Radio } from "../uiParts/Radio";
+import { Divider } from "@chakra-ui/react";
+import { H1 } from "../uiParts/heading/h1";
 
 const Home: NextPage = () => {
   const insertUser = async () => {
-    console.log("insertUser");
     await axios.post("/api/users");
   };
 
   return (
     <div>
-      <button onClick={() => insertUser()}>Insert User</button>
+      <H1>当てはまるのはだれ？</H1>
+      <Divider />
+      {/* <button onClick={() => insertUser()}>Insert User</button> */}
+      <Radio></Radio>
     </div>
   );
 };
